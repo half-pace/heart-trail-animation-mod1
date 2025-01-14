b8600daf463af3abedec9a680d887c076217b965
half pace <halfpace64@gmail.com>
2025-01-14 17:42:59 +0530

    fixed bug

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const bodyEle = document.querySelector("body");
 
 bodyEle.addEventListener("mousemove", (event) => {
-    const xPos = event.offsetX
-    const yPos = event.offsetY
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
     const spanEle = document.createElement("span");
     spanEle.style.left = xPos + "px";
-    spanEle.style.left = yPos + "px";
+    spanEle.style.top = yPos + "px";
     const size = Math.random() * 100;
     spanEle.style.width = size + "px";
     spanEle.style.height = size + "px";
