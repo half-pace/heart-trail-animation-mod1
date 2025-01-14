d206f59ef7ba48fec9fe5fec3c8331a962c5fa92
half pace <halfpace64@gmail.com>
2025-01-14 17:30:13 +0530

    functionality and final design done

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ bodyEle.addEventListener("mousemove", () => {
     const xPos = event.offsetX
     const yPos = event.offsetY
     const spanEle = document.createElement("span");
+    spanEle.style.left = xPos + "px";
+    spanEle.style.left = yPos + "px";
+    const size = Math.random() * 100;
+    spanEle.style.width = size + "px";
+    spanEle.style.height = size + "px";
     bodyEle.appendChild(spanEle);
+    setTimeout(() => {
+        spanEle.remove();
+    }, 3000);
 });
